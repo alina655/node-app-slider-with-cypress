2e777cd97ba06871fc95d0d068e21b32bf93f8eb
Alina Tadla <alina.tadla98@gmail.com>
2024-05-11 12:37:55 +0200

    commit

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,31 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if every slide contains proper title and description"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.card-description').should('be.visible').each(($cardDescription) => {
+      cy.wrap($cardDescription).find('h1').should('exist');
+      cy.wrap($cardDescription).find('p').should('exist');
+    });
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.card-description').should('be.visible').each(($cardDescription) => {
+      cy.wrap($cardDescription).find('h1').should('exist');
+      cy.wrap($cardDescription).find('p').should('exist');
+    });
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.card-description').should('be.visible').each(($cardDescription) => {
+      cy.wrap($cardDescription).find('h1').should('exist');
+      cy.wrap($cardDescription).find('p').should('exist');
+    });
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
